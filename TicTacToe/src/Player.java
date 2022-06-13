a5da8213d549736c36f11b2112fea583fcd313af
Lázaro Rocha <42587343+LazaroRochaDev@users.noreply.github.com>
2022-06-13 08:46:17 -0300

    Add files via upload

---
+++ TicTacToe/src/Player.java
@@ -0,0 +1,21 @@
+
+
+
+
+
+public class Player {
+    
+    String name;
+    char type;
+
+
+
+
+    public Player(String name, char type){
+
+        this.name = name;
+        this.type = type;
+
+    }
+
+}
